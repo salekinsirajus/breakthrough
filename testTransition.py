219df86cb53c16ade6c1affb248fd2a6df0830f4
Sirajus Salekin <ssalek14@earlham.edu>
2018-02-14 23:29:49 -0500

    Finished testing and fixing the is_valid() method

--- testTransition.py
+++ testTransition.py
@@ -3,14 +3,43 @@ from transition import Board
 
 class TestBoardMethods(unittest.TestCase):
 
-    threeX3List = [['x','x','x'],
-                        ['.','.','.'],
-                        ['o','o', 'o'],
+    def setUp(self):
+        self.threeX3List = [['x','x','x'],
+                            ['.','.','.'],
+                            ['o','o', 'o'],
+                            ]
+        self.threeX3Board = Board(self.threeX3List)
+    
+    #@unittest.skip("False value already tested")
+    def test_is_valid_false(self):
+        pairs_false = [[(0,0),(0,0)], # False, src = dst
+                        [(0,0),(0,2)],   # False, two units, same row
+                        [(0,0),(-1,0)],  # False, out of index
+                        [(0,0),(0,1)],   # False, same row
+                        [(0,0),(1,-1)],   # False, out of index
+                        [(0,0),(1,2)],   # False, moving two units
+                        [(0,0),(2,0)],   # False, moving two units
+                        [(0,0),(2,2)],   # False, moving two units
+                        [(0,0),(11,11)]   # False, out of index
                         ]
-    self.threeX3Board = Board(threeX3List)
+        for pair in pairs_false:
+            with self.subTest(pair=pair):
+                self.assertIs(self.threeX3Board.is_valid(pair[0],pair[1]), False)
 
-    def test_is_valid(self):
-        self.assertTrue(self.threeX3Board.is_valid((0,0),(1,0)))
+    def test_is_valid_true(self):
+        pairs_true = [[(0,0),(1,0)], # True, forward
+                        [(0,0),(1,1)],   # True, diagonal left
+                        [(0,1),(1,0)],  # True, diagonal left
+                        [(0,1),(1,1)],   # True, forward
+                        [(0,1),(1,2)],   # True, diagonal right
+                        [(2,0),(1,0)],   # True, forward
+                        [(2,0),(1,1)],   # True, diagonal right
+                        [(2,2),(1,2)],   # False, forward
+                        [(2,1),(1,0)]   # True, diagonal left
+                        ]
+        for pair in pairs_true:
+            with self.subTest(pair=pair):
+                self.assertTrue(self.threeX3Board.is_valid(pair[0],pair[1]))
 
 if __name__ == '__main__':
     unittest.main()
--- transition.py
+++ transition.py
@@ -16,38 +16,63 @@ class Board(object):
         # check whether the same kind or the other (opponent or own)
         try:
             (x,y) = src
-            dstSym = self.board[x][y]
             (a,b) = dst
-            srcSym = self.board[a][b]
-            dstSym = dstSym.upper() 
-            srcSym = srcSym.upper()
+            srcSym = self.get_sym(src)
+            dstSym = self.get_sym(dst)
+            print ("From get symbol method ", dstSym, srcSym)
+            if None in [srcSym, dstSym]:
+                print("Out of bound error, both negative and larger than size")
+                return False
+            print ("symbols from is_valid function ", dstSym, srcSym, src, dst)
+            # Negative indices DOES NOT raise indexError in Python
 
+            # source and destiation cannot be the same location
+            # If the direction is horizontal, return False
+            # Same applies for src = dst. This takes care of both
+            if x == a:
+                print("Error: src=dst")
+                return False
+                
             # Determine the move direction
             # If src is `O`, the valid dest direction is upwards, 
             if srcSym == self.playerO:
-                if x == (a - 1):
-                    pass
-                else:
-                    return False 
-            # for `x`
-            # the valid dest direction is downwards
-            if srcSym == self.playerX:
                 if x == (a + 1):
                     pass
                 else:
-                    return False 
+                    print("The direction is not upward")
+                    return False
+            # for `x`
+            # the valid dest direction is downwards
+            if srcSym == self.playerX:
+                if x == (a - 1):
+                    pass
+                else:
+                    print("The direction is not downward")
+                    return False
+
+            # The jump cannot be more than one unit forward or diagonal
+            col_diff = abs(y - b)
+            print ("col diff: ", col_diff)
+            if col_diff > 1 or col_diff < 0:
+                return False
+ 
             # Have the destination occupied by player of the same team
             if dstSym == srcSym:
+                print("")
                 return False
             else:
                 return True
 
-        except IndexError:
+        except IndexError as e:
+            print ("out of the board {0} {1}".format(dst, e))
             return False
     
     def get_sym(self, position):
         try:
             (x, y) = position
+            for i in position:
+                if i < 0:
+                    return None
             sym = self.board[x][y].upper()
             if sym == self.playerO:
                 return 'O' # or self.playerO
@@ -60,12 +85,62 @@ class Board(object):
         except IndexError:
             return None
 
+    def get_direction(self, posit):
+        try:
+            sym = self.get_sym(posit)
+            direction = None
+            if sym == 'O':
+                direction = 'U'
+            elif sym == 'X':
+                direction = 'D'
+            else:
+                direction = None
+            return direction
+        # Need to look have a consistent exception behavior across
+        # methods
+        except Exception:
+            return None
 
     def get_moves(self, posit):
-        pass
-     
-brd = [['x','o'],
-        ['.','.'],
+        try:
+            (x,y) = posit
+            direction = self.get_direction(posit)
+            all_moves = []
+            valid_moves = []
+            if direction == 'U':
+                print("direction is up")
+                # direction upwards, (x-1, y-1): diagonal left,
+                # (x-1, y): forward, (x-1, y+1): diagonal right
+                all_moves = [(x-1, y-1), (x-1, y), (x-1,y+1)]
+            elif direction == 'D':
+                print("direction downwards")       
+                # direction upwards, (x+1, y-1): diagonal left,
+                # (x+1, y): forward, (x+1, y+1): diagonal right
+                all_moves = [(x+1, y-1), (x+1, y), (x+1,y+1)]
+            # Think about ways to reuse this method without repeating.
+            # The following else is the crucial part
+            elif direction == '.':
+                print ("dot")
+                pass
+            else:
+                # last elif and this else can be combined
+                pass
+            # Filtering the valid moves
+            # valid_moves = list(filter(lambda x: self.is_valid(posit, x) == True, all_moves))
+            print("all_moves ", all_moves)
+            for move in all_moves:
+                if self.is_valid(posit, move) == True:
+                    valid_moves.append(move)
+                else:
+                    print("is_valid returned False")
+            return valid_moves
+        except TypeError:
+            print("Invalid position, TypeError raised.")
+            return []
+
+                         
+brd = [['x','x'],
+        ['.','.', '.'],
         ['o','o'],
         ]
 
@@ -73,4 +148,15 @@ b = Board(brd)
 print (b.is_valid((0,1), (1,1)))
 for i, r in enumerate(brd):
     for j, c in enumerate(r):
-        print(b.get_sym((i, j)))
+        
+        print("Testing is_valid method : ", b.is_valid((i,j),(i-1,j-1)), i-1, j-1)
+        print("Testing is_valid method : ", b.is_valid((i,j),(i-1,j)), i-1, j)
+        print("Testing is_valid method : ", b.is_valid((i,j),(i-1,j+1)), i-1,j+1)
+        print("Testing is_valid method : ", b.is_valid((i,j),(i+1,j-1)), i+1,j-1)
+        print("Testing is_valid method : ", b.is_valid((i,j),(i+1,j)), i+1, j)
+        print("Testing is_valid method : ", b.is_valid((i,j),(i+1,j+1)),i+1,j+1)
+        
+        print("Element in list ", i, j)
+        print("Player: ", b.get_sym((i, j)))
+        print("Direction: ", b.get_direction((i,j)))
+        print("Possible moves: ", b.get_moves((i,j)))
