f72cfb5696e23bf299149cb923d5f249624334e6
AliShahram <shahrammusavi86@gmail.com>
2018-02-15 22:06:06 -0500

    Add files via upload

--- transition.py
+++ transition.py
@@ -64,6 +64,18 @@ class Board(object):
         except IndexError as e:
             print ("out of the board {0} {1}".format(dst, e))
             return False
+
+
+    def displayState(self, input_list):
+        '''Prints out the state passed to this function on the terminal.'''
+        # check if it's a 2-d list
+        for row in input_list:
+            for column in row:
+                print(column, end=' ')
+            print("\n")
+        print("Done")
+
+
     
     def get_sym(self, position):
         try:
@@ -101,6 +113,7 @@ class Board(object):
         # methods
         except Exception:
             return None
+
 
     def get_moves(self, posit):
         try:
@@ -140,43 +153,42 @@ class Board(object):
             return []
 
 
-"""
-This is the move generator given an intial position, 
-destination and whose turn it is, it will make a single
-move from source to destination. 
+    def terminal_state(self, input_list):
+        player1 = False
+        player2 = False
 
-"""                     
-
-def single_move (src_position, dest_position, turn):
-    x = dest_position[0]
-    y = dest_position[1]
-
-    src_x = src_position[0]
-    src_y = src_position[1]
-
-    #Check whose turn it is
-    if turn == "X":  
-        if x == src_x+1 and y == src_y-1:        #direction is leftbottom
-            return src_position[0] + 1, src_position[1] -1
+        p1list = []
+        p2list = []
 
 
-        
+        for row in input_list:
+            for element in row:
+        #First case, when one of the players pieces is all out
+                if element is self.playerX:
+                    p1list.append(element)
+                if element is self.playerO:
+                    p2list.append(element)
+
+        #Second case, when one of the pieces move to the last row
+        for element in input_list[-1]:
+            if element == "X":
+                player1 = True
+        for element in input_list[0]:
+            if element == "O":
+                player2 = True
+
+        #Print the result for both cases 
+
+        if player1 is True:
+            print("Game Over. Player 1 won")
+        if player2 is True:
+            print("Game Over. Player 2 won")
 
 
-
-        
-
-
-
-
-
-source = (1,1)
-destination  = (2,0)
-turn = "X"
-
-result = single_move(source, destination, turn)
-print(result)
-
+        if len(p1list) == 0:
+            print("Game Over. Player 2 won")
+        if len(p2list) == 0:
+            print("Game Over. Player 1 won")
 
 """
 brd = [['x','x', "x"],
